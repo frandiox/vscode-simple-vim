cf5295bd53070663f0bcb06009bb9cb1708a5b4e
Fran Dios <frankdiox@gmail.com>
2019-12-25 14:31:54 +0100

    Change linter options

--- src/action_types.ts
+++ src/action_types.ts
@@ -4,7 +4,7 @@ import { VimState } from './vim_state_types';
 import { ParseKeysStatus } from './parse_keys_types';
 
 export type Action = (
-    vimState: VimState,
-    keys: string[],
-    editor: vscode.TextEditor,
+  vimState: VimState,
+  keys: string[],
+  editor: vscode.TextEditor,
 ) => ParseKeysStatus;
--- src/type_handler.ts
+++ src/type_handler.ts
@@ -5,29 +5,29 @@ import { actions } from './actions';
 import { VimState } from './vim_state_types';
 
 export function typeHandler(vimState: VimState, char: string): void {
-    const editor = vscode.window.activeTextEditor;
+  const editor = vscode.window.activeTextEditor;
 
-    if (!editor) return;
+  if (!editor) return;
 
-    vimState.keysPressed.push(char);
+  vimState.keysPressed.push(char);
 
-    try {
-        let could = false;
-        for (const action of actions) {
-            const result = action(vimState, vimState.keysPressed, editor);
+  try {
+    let could = false;
+    for (const action of actions) {
+      const result = action(vimState, vimState.keysPressed, editor);
 
-            if (result === ParseKeysStatus.YES) {
-                vimState.keysPressed = [];
-                break;
-            } else if (result === ParseKeysStatus.MORE_INPUT) {
-                could = true;
-            }
-        }
-
-        if (!could) {
-            vimState.keysPressed = [];
-        }
-    } catch (error) {
-        console.error(error);
+      if (result === ParseKeysStatus.YES) {
+        vimState.keysPressed = [];
+        break;
+      } else if (result === ParseKeysStatus.MORE_INPUT) {
+        could = true;
+      }
     }
+
+    if (!could) {
+      vimState.keysPressed = [];
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
